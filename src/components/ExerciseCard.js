97c688a727063e1846155c1cfaf0eea34ac8a0dd
med <mededdahby1@gmail.com>
2024-01-26 14:41:15 +0000

    final modifications

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Stack, Typography } from '@mui/material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
 
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-    <Stack direction="row"  alignItems='center'>
-      <Button sx={{ ml: '21px', color: '#fff', background: '#b5179e', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+    <Stack direction="row" alignItems="center">
+      <Button
+        sx={{
+          ml: "21px",
+          color: "#fff",
+          background: "#b5179e",
+          fontSize: "14px",
+          borderRadius: "20px",
+          textTransform: "capitalize",
+        }}
+      >
         {exercise.bodyPart}
       </Button>
-      <Button sx={{ ml: '21px', color: '#fff', background: '#ffbc42', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+      <Button
+        sx={{
+          ml: "21px",
+          color: "#fff",
+          background: "#ffbc42",
+          fontSize: "14px",
+          borderRadius: "20px",
+          textTransform: "capitalize",
+        }}
+      >
         {exercise.target}
       </Button>
     </Stack>
-    <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
+    <Typography
+      ml="21px"
+      color="#000"
+      fontWeight="bold"
+      sx={{ fontSize: { lg: "24px", xs: "20px" } }}
+      mt="11px"
+      pb="10px"
+      textTransform="capitalize"
+    >
       {exercise.name}
     </Typography>
   </Link>
